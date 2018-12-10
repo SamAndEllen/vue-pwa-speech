7a8026d798fda63a5d403b8c20003dd5d270c230
existmaster <existmaster@naver.com>
2018-12-10 23:55:21 +0900

    원복

--- src/components/FormEval.vue
+++ src/components/FormEval.vue
@@ -144,7 +144,7 @@
         this.loader = true;
         // create WAV download link using audio data blob
         this.processRecording();
-        //this.createDownloadLink();
+        this.createDownloadLink();
       },
       createDownloadLink() {
         const au = document.getElementById('reload');
@@ -161,17 +161,13 @@
         const vm = this;
 
         recorder && recorder.exportWAV(function(blob) {
-          console.log(blob);
           var reader = new window.FileReader();
           reader.readAsDataURL(blob);
           reader.onloadend = () => {
             const baseData = reader.result;
             const base64Data = baseData.replace("data:audio/wav;base64,", "");
-            console.log(baseData);
-            console.log(base64Data);
             vm.data.audio.content = base64Data;
             vm.data.config.languageCode = vm.selected;
-            console.log(vm.data);
             axios.post(
               `https://speech.googleapis.com/v1/speech:recognize?key=${vm.apiKey}`,
               vm.data).then(response => {
@@ -203,103 +199,29 @@
       },
       redirectError() {
         window.location.href = "http://localhost:8080/"
-      },
-      gotStream(stream) {
-    inputPoint = audio_context.createGain();
-
-    // Create an AudioNode from the stream.
-    realAudioInput = audio_context.createMediaStreamSource(stream);
-    audioInput = realAudioInput;
-    audioInput.connect(inputPoint);
-
-//    audioInput = convertToMono( input );
-
-    analyserNode = audio_context.createAnalyser();
-    analyserNode.fftSize = 2048;
-    inputPoint.connect( analyserNode );
-
-    audioRecorder = new Recorder( inputPoint );
-
-    zeroGain = audio_context.createGain();
-    zeroGain.gain.value = 0.0;
-    inputPoint.connect( zeroGain );
-    zeroGain.connect( audio_context.destination );
-    updateAnalysers();
-
-},
- updateAnalysers(time) {
-    if (!analyserContext) {
-        var canvas = document.getElementById("analyser");
-        canvasWidth = canvas.width;
-        canvasHeight = canvas.height;
-        analyserContext = canvas.getContext('2d');
-    }
-
-    // analyzer draw code here
-    {
-        var SPACING = 3;
-        var BAR_WIDTH = 1;
-        var numBars = Math.round(canvasWidth / SPACING);
-        var freqByteData = new Uint8Array(analyserNode.frequencyBinCount);
-
-        analyserNode.getByteFrequencyData(freqByteData); 
-
-        analyserContext.clearRect(0, 0, canvasWidth, canvasHeight);
-        analyserContext.fillStyle = '#F6D565';
-        analyserContext.lineCap = 'round';
-        var multiplier = analyserNode.frequencyBinCount / numBars;
-
-        // Draw rectangle for each frequency bin.
-        for (var i = 0; i < numBars; ++i) {
-            var magnitude = 0;
-            var offset = Math.floor( i * multiplier );
-            // gotta sum/average the block, or we miss narrow-bandwidth spikes
-            for (var j = 0; j< multiplier; j++)
-                magnitude += freqByteData[offset + j];
-            magnitude = magnitude / multiplier;
-            var magnitude2 = freqByteData[i * multiplier];
-            analyserContext.fillStyle = "hsl( " + Math.round((i*360)/numBars) + ", 100%, 50%)";
-            analyserContext.fillRect(i * SPACING, canvasHeight, BAR_WIDTH, -magnitude);
-        }
-    }
-    
-    rafID = window.requestAnimationFrame( updateAnalysers );
-}
+      }
     },
     created() {
       this.$emit('update:headtitle', '형성평가');
       try {
         window.AudioContext = window.AudioContext || window.webkitAudioContext;
+        navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia;
+        window.URL = window.URL || window.webkitURL;
   
-        audio_context = new AudioContext();
+        audio_context = new AudioContext;
         console.log('Audio context set up.');
         console.log('navigator.getUserMedia ' + (navigator.getUserMedia ? 'available.' : 'not present!'));
       } catch (e) {
         alert('No web audio support in this browser!');
       }
       const that = this;
-      // navigator.getUserMedia({
-      //   audio: true
-      // }, function(stream) {
-      //   that.startUserMedia(stream)
-      // }, function(e) {
-      //   console.log('No live audio input: ' + e);
-      // });
-          navigator.getUserMedia(
-        {
-            "audio": {
-                "mandatory": {
-                    "googEchoCancellation": "false",
-                    "googAutoGainControl": "false",
-                    "googNoiseSuppression": "false",
-                    "googHighpassFilter": "false"
-                },
-                "optional": []
-            },
-        }, this.gotStream, function(e) {
-            alert('Error getting audio');
-            console.log(e);
-        });
+      navigator.getUserMedia({
+        audio: true
+      }, function(stream) {
+        that.startUserMedia(stream)
+      }, function(e) {
+        console.log('No live audio input: ' + e);
+      });
 
       this.question = this.$route.query.ctx;
     }
